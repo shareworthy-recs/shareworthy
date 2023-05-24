050abcce4703366c4a595812973cb6cc15621b1c
Jake R <jwr318@gmail.com>
2023-05-23 21:34:42 -0400

    set up package JSON and webpack...needs to be tested

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  target: 'web',
+  devServer: {
+    port: 8080,
+    host: 'localhost',
+    open: true,
+    hot: true,
+    liveReload: true,
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+      },
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env', '@babel/react'],
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      },
+    ],
+  },
+};
