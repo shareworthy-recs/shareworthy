5981e976775443cd93122ab12e0eae601edb3daa
Jake R <jwr318@gmail.com>
2023-05-24 22:55:16 -0400

    updated signin component and login componenet

--- client/components/pages/Login.tsx
+++ client/components/pages/Login.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
 const Login = () => {
-   return (
-     <>
-       <div>
-         <div>Signup</div>
-         <form>
-           <div>
-             <input type="email" placeholder="Enter your email" />
-           </div>
-           <div>
-             <input type="password" placeholder="Enter your password" />
-           </div>
-           <div>
-             <button type="submit">Join</button>
-           </div>
-         </form>
-         <div>
-           <button>Continue with Google</button>
-         </div>
-         <div>
-           <p>
-             Already on Shareworthy? <a href="/Login">Sign in</a>
-           </p>
-         </div>
-       </div>
-     </>
-   );
+  return (
+    <>
+      <div>
+        <div>Sign In</div>
+        <form>
+          <div>
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+        <div>
+          <button>Continue with Google</button>
+        </div>
+        <div>
+          <button>Sign in with Github</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
