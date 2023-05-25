8019ba09c259deac410a39e22b37e8d81adba621
Jason Brown <superbunker@gmail.com>
2023-05-24 19:52:35 -0700

    configuring test libraries

---
+++ client/App.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('this test should fail', () => {
+    test('2 + 2 ≠ 5', () => {
+      const fail = 2 + 2;
+      expect(fail).toBe(5);
+    });
+  });
+
+describe('this test should pass', () => {
+    test('2 + 2 = 4', () => {
+    const pass = 2 + 2;
+    expect(pass).toBe(4);
+    });
+});
+
+
+test('renders the landing page', () => {
+  render(<App />);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+});
