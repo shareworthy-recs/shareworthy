9fa4a71e260aade6e23aed0187256c80506bff96
Jake R <jwr318@gmail.com>
2023-06-01 17:40:22 -0400

    removed github oauth

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,6 @@ import session from 'express-session';
 import passport from 'passport';
 import {
   initializeGoogleStrategy,
-  initializeGithubStrategy,
   initializeUserSerialization,
 } from './passport';
 import authRoutes from './routes/authRoutes';
@@ -36,7 +35,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 initializeGoogleStrategy();
-initializeGithubStrategy();
 initializeUserSerialization();
 
 app.use('/', express.static(path.resolve(__dirname, '../client')));
